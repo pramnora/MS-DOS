3447e4505e36d633d9885e6e19ccea0de386e0b1
Paul Ramnora <pramnora@yahoo.com>
2022-08-10 10:48:55 +0100

    Create comment01.bat
    Added file: [comment01.bat] Demonstrates the use of MS DOS comments...

---
+++ language/comments/comment01.bat
@@ -0,0 +1,8 @@
+@ECHO OFF
+REM *** This is a MS DOS comment
+REM     When you run it...you won't be able to see any output
+REM     as comments are not executable code
+REM     but, instead, are used to explain using just 'plain english text'
+REM     to anyone who is reading the code...what that code is meant to be doing.
+PAUSE
+
