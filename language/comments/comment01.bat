e074aaa932100557908b893932c04b27e48b5be4
Paul Ramnora <pramnora@yahoo.com>
2022-08-10 10:52:17 +0100

    Update comment01.bat
    Extended multi-line comment...to read a lot more clear

--- language/comments/comment01.bat
+++ language/comments/comment01.bat
@@ -1,8 +1,10 @@
 @ECHO OFF
-REM *** This is a MS DOS comment
-REM     When you run it...you won't be able to see any output
-REM     as comments are not executable code
-REM     but, instead, are used to explain using just 'plain english text'
-REM     to anyone who is reading the code...what that code is meant to be doing.
+REM *** This is a MS DOS REMARK comment.
+REM     When you run this file...you won't be able to see any visible 'output'...;
+REM     as comments are NOT executable code;
+REM     but, instead, are used to help explain using just 
+REM     'plain straight forwards english text'
+REM     to anyone who is reading what is the program's underlying 'source code'...
+REM     what that code is actually meant to be doing.
 PAUSE
 
